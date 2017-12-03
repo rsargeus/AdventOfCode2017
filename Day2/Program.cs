9bfa1146644cf5a9245c0843898def910605ef74
Robert Sargeus <Robert.Sargeus@ikano.se>
2017-12-03 20:28:11 +0100

    Completed day 2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -18,16 +18,41 @@ namespace Day2
                 string[] stringValues = line.Split('\t');
                 int[] numbers = Array.ConvertAll(stringValues, s => int.Parse(s));
 
-                sum+= GetDiffPartOne(numbers);                
+                //Part One
+                //sum+= GetNumberPartOne(numbers);
+
+                //Part Two
+                sum+= GetNumberPartTwo(numbers);                
             }  
                         
             // Suspend the screen.  
             Console.WriteLine(sum);
         }
 
-        private static int GetDiffPartOne(int[] values)
+        private static int GetNumberPartOne(int[] values)
         {            
-            return values.Max() - values.Min();;
+            return values.Max() - values.Min();
+        }   
+
+        private static int GetNumberPartTwo(int[] values)
+        {            
+            values = values.OrderByDescending(i => i).ToArray();
+
+            int start = 1;
+            for(int i=0;i<values.Length-1; i++)
+            {
+                for(int j=start;j<values.Length; j++)
+                {
+                    if(values[i] % values[j] == 0)
+                    {
+                        return values[i] / values[j];
+                    }
+                }
+
+                start++;
+            }
+
+            throw new InvalidOperationException("Could not find a match");
         }        
     }
 }
