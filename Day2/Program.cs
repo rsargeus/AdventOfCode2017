bdb66ef7b8367d903e5271a5afdf25217c662a67
Robert Sargéus <robert.sargeus@geshdo.com>
2017-12-03 16:29:23 +0100

    Added Day 2 (part one)

---
+++ Day2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            
+            string line;  
+            // Read the file and display it line by line.  
+            using(StreamReader file =  new StreamReader(@"input.txt"))  
+            while((line = file.ReadLine()) != null)  
+            {  
+                string[] stringValues = line.Split('\t');
+                int[] numbers = Array.ConvertAll(stringValues, s => int.Parse(s));
+
+                sum+= GetDiffPartOne(numbers);                
+            }  
+                        
+            // Suspend the screen.  
+            Console.WriteLine(sum);
+        }
+
+        private static int GetDiffPartOne(int[] values)
+        {            
+            return values.Max() - values.Min();;
+        }        
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 https://adventofcode.com/2017/
 
 Day 1 : Part One:1341, Part two:1348 (dotnet webapi)
+
+Day 2 : Part One: The checksum is 34925 (dotnet console)
+
